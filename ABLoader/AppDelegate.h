22ba12047422965914d502a73e78c93c05e8f040
PAOLO MUSOLINO <info@codeido.com>
2015-07-06 11:48:46 +0200

    Initial Commit

---
+++ ABLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABLoader
+//
+//  Created by Paolo Musolino on 06/07/15.
+//  Copyright (c) 2015 IQUII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ABLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ABLoader
+//
+//  Created by Paolo Musolino on 06/07/15.
+//  Copyright (c) 2015 IQUII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
